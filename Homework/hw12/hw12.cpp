40d1cb8f8985aee7ac9c825a378fef4cac248705
Ethan Malabago <malabagoethan@gmail.com>
2025-11-13 17:07:34 -0500

    finished hw12

--- Homework/hw12/hw12.cpp
+++ Homework/hw12/hw12.cpp
@@ -17,11 +17,11 @@ const int SIZE = 100;
 
 unsigned int seed = (unsigned int) clock();
 
-double random(unsigned int &seed);
-
 struct Point;
+double random(unsigned int &seed);
 void fill_array(Point a[]);
-double areaCheck(Point a[]);
+double area_check(const Point a[]);
+double distance_calc(Point p_1, Point p_2);
 
 struct Point
 {
@@ -30,6 +30,13 @@ struct Point
 
 int main()
 {
+    Point points[SIZE];
+
+    fill_array(points);
+
+    cout << fixed << setprecision(12) << endl;
+    cout << "smallest area: " << area_check(points) << endl;
+    cout << endl;
 
     return 0;
 }
@@ -43,6 +50,45 @@ double random(unsigned int &seed)
     seed = ((MULTIPLIER * seed) + INCREMENT) % MODULUS;
 
     return double(seed) / MODULUS;
+}
+
+void fill_array(Point points[])
+{
+    for (int i = 0; i < SIZE; i++)
+    {
+        points[i].x = random(seed);
+        points[i].y = random(seed);
+    }
+}
+
+double area_check(const Point points[])
+{
+    double area = 50;
+    double new_area, s, a, b, c;
+    for (int i = 0; i < SIZE; i++)
+    {
+        for (int j = i + 1; j < SIZE; j++)
+        {
+            for (int k = j + 1; k < SIZE; k++)
+            {
+                a = distance_calc(points[i], points[j]);
+                b = distance_calc(points[j], points[k]);
+                c = distance_calc(points[i], points[k]);
+                s = (a + b + c) / 2;
+                new_area = sqrt(s * (s - a) * (s - b) * (s - c));
+
+                if (new_area < area)
+                    area = new_area;
+            }
+        }
+    }
+
+    return area;
+}
+
+double distance_calc(Point p_1, Point p_2)
+{
+    return sqrt( pow((p_2.x - p_1.x), 2.0) + pow((p_2.y - p_1.y), 2.0) );
 }
 
 /*
